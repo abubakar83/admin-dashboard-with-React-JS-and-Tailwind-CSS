db2c0f3adc5ca9f3807a52476bdfe10aa6b4c045
abubakar83 <abubakar.abs83@gmail.com>
2025-08-24 05:59:36 +0500

    sidebar layout

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import Sidebar from './components/Layout/Sidebar'
 
 const App = () => {
   return (
-    <div className="main-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all-duration-500">
+    <div className="main-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500">
       <div className="flex h-screen overflow-hidden">
         <Sidebar/>
       </div>
