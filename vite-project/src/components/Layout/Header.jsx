4f4563110d211845ea94e242ce0aceebb41593a6
abubakar83 <abubakar.abs83@gmail.com>
2025-08-26 19:18:11 +0500

    add filter button in serach box

--- vite-project/src/components/Layout/Header.jsx
+++ vite-project/src/components/Layout/Header.jsx
@@ -1,4 +1,4 @@
-import { Menu, Search } from "lucide-react";
+import { Filter, Menu, Search } from "lucide-react";
 
 const Header = () => {
   return (
@@ -25,6 +25,9 @@ const Header = () => {
               placeholder="Search Anything"
               className="w-full pl-10 pr-4 py-2.5 bg-slate-100 dark:bg-slate-800 border border-slate-200  dark:border-slate-700 rounded-xl text-slate-800 dark:text-white placeholder-slate-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
             />
+            <button className="absolute right-2 top-1/2 transform -translate-y-1/2 p-1.5 text-slate-400 hover:text-slate-600 dark:hover:text-slate-300">
+              <Filter/>
+            </button>
           </div>
         </div>
       </div>
