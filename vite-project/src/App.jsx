a83e9c2b2ba754797411ef676558cabe9d6e44fd
abubakar83 <abubakar.abs83@gmail.com>
2025-08-28 08:21:52 +0500

    start Dashborad

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,11 @@
 import Sidebar from "./components/Layout/Sidebar";
 import Header from "./components/Layout/Header";
 import { useState } from "react";
+import Dashboard from "./Dashboard/Dashboard";
 
 const App = () => {
   const [sideBarCollapsed, setSideBarCollapsed] = useState(false);
-  const [currentPage, setCurrentPage] = useState("dasboard");
+  const [currentPage, setCurrentPage] = useState("dashboard");
   return (
     <div className="main-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500">
       <div className="flex h-screen overflow-hidden">
@@ -19,6 +20,12 @@ const App = () => {
             sidebarCollapsed={sideBarCollapsed}
             onToggleSidebar={() => setSideBarCollapsed(!sideBarCollapsed)}
           />
+
+          <main className="flex-1 overflow-y-auto bg-transparent">
+            <div className="p-6 space-y-6">
+              {currentPage === "dashboard" && <Dashboard />}
+            </div>
+          </main>
         </div>
       </div>
     </div>
--- vite-project/src/components/Layout/Sidebar.jsx
+++ vite-project/src/components/Layout/Sidebar.jsx
@@ -148,22 +148,22 @@ const Sidebar = ({ collapsed, onToggle, currentPage, onPageChange }) => {
                 <div className="flex items-center space-x-3">
                   <item.icon className={`w-5 h-5`} />
                   {/* Conditional Rendering */}
-                  
-                    {!collapsed && (
-                      <>
+
+                  {!collapsed && (
+                    <>
                       <span className="font-medium ml-2">{item.label}</span>
                       {item.badge && (
-                      <span className="px-2 py-1 text-xs bg-red-500 text-white rounded-full ">
-                        {item.badge}
-                      </span>
-                    )}
-                    {item.count && (
-                      <span className="px-2 py-1 text-xs bg-slate-200 dark:bg-slate-700 text-slate-600 dark:text-slate-300 rounden-full">
-                        {item.count}
-                      </span>
-                    )}
+                        <span className="px-2 py-1 text-xs bg-red-500 text-white rounded-full ">
+                          {item.badge}
+                        </span>
+                      )}
+                      {item.count && (
+                        <span className="px-2 py-1 text-xs bg-slate-200 dark:bg-slate-700 text-slate-600 dark:text-slate-300 rounden-full">
+                          {item.count}
+                        </span>
+                      )}
                     </>
-                    )}
+                  )}
                 </div>
                 {!collapsed && item.submenu && (
                   <ChevronDown className={`w-4 h-4 transition-transform `} />
