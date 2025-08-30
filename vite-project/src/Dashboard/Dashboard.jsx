da9525860771a1dd4c052970663404e8b60819dd
abubakar83 <abubakar.abs83@gmail.com>
2025-08-30 13:49:42 +0500

    styling TableSection and add static data

--- vite-project/src/Dashboard/Dashboard.jsx
+++ vite-project/src/Dashboard/Dashboard.jsx
@@ -1,23 +1,23 @@
-import ChartSection from "./ChartSection"
-import StatsGrid from "./StatsGrid"
-import TableSection from "./TableSection"
+import ChartSection from "./ChartSection";
+import StatsGrid from "./StatsGrid";
+import TableSection from "./TableSection";
 
 const Dashboard = () => {
   return (
     <div className="space-y-6">
-        {/* Stats grid */}
-        <StatsGrid/>
+      {/* Stats grid */}
+      <StatsGrid />
 
-        {/* Charts Section */}
-        <ChartSection/>
+      {/* Charts Section */}
+      <ChartSection />
 
-        <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
         <div className="xl:col-span-2">
-      <TableSection/>
+          <TableSection />
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
