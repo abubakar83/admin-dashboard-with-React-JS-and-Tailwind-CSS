5fc42b74ac1128e6f9a2b460a769387aa3435417
abubakar83 <abubakar.abs83@gmail.com>
2025-08-30 11:37:31 +0500

    create TableSection Dashboard component

--- vite-project/src/Dashboard/Dashboard.jsx
+++ vite-project/src/Dashboard/Dashboard.jsx
@@ -1,12 +1,21 @@
 import ChartSection from "./ChartSection"
 import StatsGrid from "./StatsGrid"
+import TableSection from "./TableSection"
 
 const Dashboard = () => {
   return (
     <div className="space-y-6">
         {/* Stats grid */}
         <StatsGrid/>
+
+        {/* Charts Section */}
         <ChartSection/>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+        <div className="xl:col-span-2">
+      <TableSection/>
+        </div>
+        </div>
     </div>
   )
 }
