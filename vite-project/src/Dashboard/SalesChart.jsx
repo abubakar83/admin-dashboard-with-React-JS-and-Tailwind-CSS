eb24ffe78933440e942f02b111837b9cc4ae28e3
abubakar83 <abubakar.abs83@gmail.com>
2025-08-30 10:36:14 +0500

    add some extreadata in PieChart by the help of map method

--- vite-project/src/Dashboard/SalesChart.jsx
+++ vite-project/src/Dashboard/SalesChart.jsx
@@ -12,10 +12,10 @@ const SalesChart = () => {
     <div className="bg-white dark:bg-slate-900 backdrop-blur-xl rounded-b-2xl p-6 border border-slate-200/50 dark:border-slate-700/50 ">
       <div className="mb-6">
         <h3 className="text-lg font-bold text-slate-800 dark:text-white">
-          Sales by Cateogry
+          Sales by Category
         </h3>
         <p className="text-sm text-slate-500 dark:text-slate-400">
-          Production Distrbution
+          Production Distribution
         </p>
       </div>
       <div className="h-48">
@@ -41,9 +41,29 @@ const SalesChart = () => {
                 borderRadius: "12px",
                 boxShadow: "0 10px 40px rgba(0, 0, 0, 0.1)",
               }}
-            ></Tooltip>
+            />
           </PieChart>
         </ResponsiveContainer>
+      </div>
+      <div className="space-y-3">
+        {data.map((item, index) => {
+          return (
+            <div className="flex items-center justify-between" key={index}>
+              <div className="flex items-center space-x-3 ">
+                <div
+                  className="w-3 h-3 rounded-full"
+                  style={{ backgroundColor: item.color }}
+                ></div>
+                <span className="text-sm text-slate-600 dark:text-slate-400">
+                  {item.name}
+                </span>
+              </div>
+              <div className="text-sm font-semibold text-slate-800 dark:text-white">
+                {item.value}%
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
