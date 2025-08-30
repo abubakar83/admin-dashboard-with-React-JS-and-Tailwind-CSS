a7825366d60eeda163ea193ae789e2ce8a738ddf
abubakar83 <abubakar.abs83@gmail.com>
2025-08-30 09:48:17 +0500

    displaying object data with responsive container and iterate map method on it

--- vite-project/src/Dashboard/SalesChart.jsx
+++ vite-project/src/Dashboard/SalesChart.jsx
@@ -1,24 +1,52 @@
-import { Pie, PieChart, Cell, ResponsiveContainer, Tooltip } from "recharts"
+import { Pie, PieChart, Cell, ResponsiveContainer, Tooltip } from "recharts";
 
 const data = [
-        {name: "Electronics", value: 45, color: "#3b82f6"},
-        {name: "Clothing", value: 30, color: "#8b5cf6"},
-        {name: "Books", value: 15, color: "#10b981"},
-        {name: "Others", value: 10, color: "#f59e0b"},
-    ]
+  { name: "Electronics", value: 45, color: "#3b82f6" },
+  { name: "Clothing", value: 30, color: "#8b5cf6" },
+  { name: "Books", value: 15, color: "#10b981" },
+  { name: "Others", value: 10, color: "#f59e0b" },
+];
 
 const SalesChart = () => {
   return (
     <div className="bg-white dark:bg-slate-900 backdrop-blur-xl rounded-b-2xl p-6 border border-slate-200/50 dark:border-slate-700/50 ">
-        <div className="mb-6">
-        <h3 className="text-lg font-bold text-slate-800 dark:text-white">Sales by Cateogry</h3>
-        <p className="text-sm text-slate-500 dark:text-slate-400">Production Distrbution</p>
-        </div>
-        <div className='h-48'>
-
-        </div>
+      <div className="mb-6">
+        <h3 className="text-lg font-bold text-slate-800 dark:text-white">
+          Sales by Cateogry
+        </h3>
+        <p className="text-sm text-slate-500 dark:text-slate-400">
+          Production Distrbution
+        </p>
+      </div>
+      <div className="h-48">
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              innerRadius={40}
+              outerRadius={80}
+              paddingAngle={5}
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.color} />
+              ))}
+            </Pie>
+            <Tooltip
+              contentStyle={{
+                backgroundColor: "rgba(255, 255, 255, 0.95)",
+                border: "none",
+                borderRadius: "12px",
+                boxShadow: "0 10px 40px rgba(0, 0, 0, 0.1)",
+              }}
+            ></Tooltip>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SalesChart
+export default SalesChart;
